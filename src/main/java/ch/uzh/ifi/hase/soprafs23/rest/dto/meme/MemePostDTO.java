217161153511a012cfa0850fcad9c5b21bef58fe
hnord-vdx <carlhenrikklaes.nordgren@uzh.ch>
2023-04-25 18:14:34 +0200

    Added color and font to meme

--- src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/meme/MemePostDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/meme/MemePostDTO.java
@@ -6,7 +6,35 @@ import ch.uzh.ifi.hase.soprafs23.entity.TextBox;
 
 public class MemePostDTO {
 
+    private String color;
+    private int currentRound;
+    private int fontSize;
+
     private List<TextBox> textBoxes;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getCurrentRound() {
+        return currentRound;
+    }
+
+    public void setCurrentRound(int currentRound) {
+        this.currentRound = currentRound;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+    }
 
     public List<TextBox> getTextBoxes() {
         return textBoxes;
