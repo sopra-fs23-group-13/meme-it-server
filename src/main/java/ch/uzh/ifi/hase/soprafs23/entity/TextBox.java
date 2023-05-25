8524226118d26d057fee1033071614a718713d42
hnord-vdx <carlhenrikklaes.nordgren@uzh.ch>
2023-05-25 15:18:41 +0200

    Made TextBox serialisable

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/TextBox.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/TextBox.java
@@ -2,11 +2,15 @@ package ch.uzh.ifi.hase.soprafs23.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "TEXTBOX")
-public class TextBox {
+public class TextBox implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
